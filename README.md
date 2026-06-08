7a6162a5dd835055fa9158d5441d4dcb5f1bf16e
Клепиков Андрей <a.klepikov@dssl.ru>
2026-06-08 12:52:03 +0300

    Sprint_4

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# qa_python
+# Sprint_4
+
+## Реализованные тесты
+
+| # | Тестируемый метод | Тестовый метод | Покрываемый сценарий | Комментарий  |
+|---|-------------------|----------------|----------------------|--------------|
+| 1 |  add_new_book     |  test_add_new_book_add_two_books| Добавление 2 книг | Пример (кривой, т.к. использует несуществующий метод) |
+| 2 |  add_new_book     | test_add_new_book_incorrect_length_not_added | Книги с некорректной длиной не добавляются | Негатив |
+| 3 |  add_new_book     | test_add_new_book_duplicate_not_added | Дубликат не добавляется | Негатив |
+| 4 |  set_book_genre   | test_set_book_genre_set_existing_genre | Установка существующего жанра для книги проходит успешно |   |
+| 5 |  set_book_genre   | test_set_book_genre_set_non_existing_genre_fail  | Несуществующий жанр не устанавливается | Негатив |
+| 6 |  get_book_genre   | test_get_book_genre_returns_genre | Возвращение жанра по названию книги работает |   |
+| 7 | get_books_with_specific_genre | test_get_books_with_specific_genre_existing_book_returns_book | Возвращает 1 книгу по существующему жанру |   |
+| 8 | get_books_with_specific_genre | test_get_books_with_specific_genre_incorrect_genre_returns_empty_list | По несуществующему жанру не возвращаются книги | Негатив |
+| 9 | get_books_genre   | test_get_books_genre_add_2_books_return_2_books | Корректно отдаёт дикт с 2 книгами с заданным жанром |   |
+| 10 | get_books_genre   | test_get_books_genre_no_books_return_empty_dict | Корректно отдаёт пустой дикт, если книг не добавлено |   |
+| 11 | get_books_for_children | test_get_books_for_children_add_children_books_return_children_books | Отдаёт 1 книгу с детским жанром |   |
+| 12 | get_books_for_children | test_get_books_for_children_add_non_children_books_return_empty_list | Отдаёт пустой список, если добавлены только книги с ограничением по возрасту | Негатив |
+| 13 | add_book_in_favorites | test_add_book_in_favorites_add_1_fav_book_return_1_fav_book | Добавляет 1 книгу в избранное, она появляется в списке избранных |   |
+| 14 | delete_book_from_favorites | test_delete_book_from_favorites_add_1_delete_1_return_empty_favlist | Удаляет добавленную в избранное книгу, проверяет пустой ли список избранного |   |
+| 15 | get_list_of_favorites_books | test_get_list_of_favorites_books_added_2_fav_return_2_fav | Добавляет 2 книги в избранное, отдаёт список избранного |   |
+
+## Нереализованные тесты
+
+| # | Тестируемый метод | Покрываемый сценарий |
+|---|-------------------|----------------------|
+| 1 | add_new_book      | Добавление книг (тест из примера кривой) |
+| 2 | get_books_with_specific_genre | Запрос по жанрам при наличии нескольких книг с разными жанрами |
+| 3 | get_books_for_children | Запрос книг для детей при наличии книг для детей и не для детей в дикте |
+| 4 | add_book_in_favorites | Добавление нескольких книг в избранное |
+| 5 | delete_book_from_favorites | Удаление части книг из избранного |
--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
+import pytest
+
 from main import BooksCollector
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -22,3 +25,185 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+    @pytest.mark.parametrize(
+        'book_name', [
+            '',
+            'Жизнь и удивительные приключения Робинзона Крузо, моряка из Йорка, '
+            'прожившего двадцать восемь лет в полном одиночестве на необитаемом '
+            'острове у берегов Америки, близ устьев реки Ориноко, куда он был '
+            'выброшен кораблекрушением, во время которого весь экипаж корабля, '
+            'кроме него, погиб, с изложением его неожиданного освобождения пиратами; '
+            'написанные им самим'
+        ],
+        ids=['empty len', 'over 40 len']
+    )
+    def test_add_new_book_incorrect_length_not_added(self,book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+
+        assert not collector.get_books_genre()
+
+
+    def test_add_new_book_duplicate_not_added(self):
+        collector = BooksCollector()
+        collector.add_new_book('Возвышение Хоруса')
+        collector.add_new_book('Возвышение Хоруса')
+
+        assert len(collector.get_books_genre()) == 1
+
+
+    @pytest.mark.parametrize(
+        'book_name,book_genre',
+        [('Лживые Боги', 'Комедии'), ('Полёт Эйзенштейна', 'Мультфильмы')],
+        ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
+    )
+    def test_set_book_genre_set_existing_genre(self, book_name, book_genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+
+        assert collector.get_book_genre(book_name) == book_genre
+
+
+    def test_set_book_genre_set_non_existing_genre_fail(self):
+        collector = BooksCollector()
+        collector.add_new_book('Лживые Боги')
+        collector.set_book_genre('Лживые Боги', 'Сборник рецептов')
+
+        assert collector.get_book_genre('Лживые Боги') == ''
+
+
+    @pytest.mark.parametrize(
+        'book_name,book_genre',
+        [('Лживые Боги', 'Комедии'), ('Полёт Эйзенштейна', 'Мультфильмы')],
+        ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
+    )
+    def test_get_book_genre_returns_genre(self,book_name, book_genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+        returned_genre = collector.get_book_genre(book_name)
+
+        assert returned_genre == book_genre
+
+
+    @pytest.mark.parametrize(
+        'book_name,book_genre',
+        [('Лживые Боги', 'Комедии'), ('Полёт Эйзенштейна', 'Мультфильмы')],
+        ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
+    )
+    def test_get_books_with_specific_genre_existing_book_returns_book(
+            self, book_name, book_genre
+    ):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+        books_by_genre = collector.get_books_with_specific_genre(book_genre)
+
+        assert books_by_genre == [book_name]
+
+
+    @pytest.mark.parametrize(
+        'book_name, book_genre',
+        [('Лживые Боги', 'Гримдарк'), ('Полёт Эйзенштейна', 'Эпос')],
+        ids=['False Gods + Grimdark','The Flight of the Eisenstein + Epic Poetry']
+    )
+    def test_get_books_with_specific_genre_incorrect_genre_returns_empty_list(self, book_name, book_genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+        books_by_genre = collector.get_books_with_specific_genre(book_genre)
+
+        assert books_by_genre == []
+
+
+    @pytest.mark.parametrize(
+        'bookngenre',
+        [
+            (
+                    {'Лживые Боги': 'Комедии',
+                     'Полёт Эйзенштейна': 'Мультфильмы'}
+            ),
+            (
+                    {'Возвышение Хоруса': 'Детективы',
+                     'Тысяча Сынов': 'Комедии'}
+            )
+        ],
+        ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
+    )
+    def test_get_books_genre_add_2_books_return_2_books(self, bookngenre):
+        collector = BooksCollector()
+        for k, v in bookngenre.items():
+            collector.add_new_book(k)
+            collector.set_book_genre(k, v)
+        books_by_genre = collector.get_books_genre()
+
+        assert books_by_genre == bookngenre
+
+
+    def test_get_books_genre_no_books_return_empty_dict(self):
+        collector = BooksCollector()
+        books_by_genre = collector.get_books_genre()
+
+        assert books_by_genre == {}
+
+
+    @pytest.mark.parametrize(
+        'book_name,book_genre',
+        [('Лживые Боги', 'Комедии'), ('Полёт Эйзенштейна', 'Мультфильмы')],
+        ids=['Comedy','Animation']
+    )
+    def test_get_books_for_children_add_children_books_return_children_books(self, book_name, book_genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+        children_books = collector.get_books_for_children()
+
+        assert children_books == [book_name]
+
+
+    @pytest.mark.parametrize(
+        'book_name,book_genre',
+        [('Где Уолли?', 'Детективы'), ('Дом на Пуховой опушке', 'Ужасы')],
+        ids=['Detective', 'Horror']
+    )
+    def test_get_books_for_children_add_non_children_books_return_empty_list(self, book_name, book_genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+        children_books = collector.get_books_for_children()
+
+        assert children_books == []
+
+
+    def test_add_book_in_favorites_add_1_fav_book_return_1_fav_book(self):
+        collector = BooksCollector()
+        book_name = 'Сожжение Просперо'
+        collector.add_new_book(book_name)
+        collector.add_book_in_favorites(book_name)
+
+        assert collector.favorites == [book_name]
+
+
+    def test_delete_book_from_favorites_add_1_delete_1_return_empty_favlist(self):
+        collector = BooksCollector()
+        book_name = 'Сожжение Просперо'
+        collector.add_new_book(book_name)
+        collector.add_book_in_favorites(book_name)
+        collector.delete_book_from_favorites(book_name)
+
+        assert collector.favorites == []
+
+
+    def test_get_list_of_favorites_books_added_2_fav_return_2_fav(self):
+        collector = BooksCollector()
+        book1_name = 'Сожжение Просперо'
+        book2_name = 'Фулгрим'
+        collector.add_new_book(book1_name)
+        collector.add_book_in_favorites(book1_name)
+        collector.add_new_book(book2_name)
+        collector.add_book_in_favorites(book2_name)
+
+        assert collector.get_list_of_favorites_books() == [book1_name, book2_name]
