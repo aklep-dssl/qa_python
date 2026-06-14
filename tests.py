9cef1586ad0f41758c7a85edd05d2c0410bf4525
Клепиков Андрей <a.klepikov@dssl.ru>
2026-06-14 23:35:45 +0300

    Sprint_4: fix pre-made test; add new BVA test; fix parametrization for test_add_new_book_incorrect_length_not_added; fix parametrization for test_get_books_genre_add_2_books_return_2_books

--- tests.py
+++ tests.py
@@ -1,7 +1,5 @@
 import pytest
 
-from main import BooksCollector
-
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -11,9 +9,7 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_add_two_books(self, collector):
 
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
@@ -21,7 +17,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
@@ -30,24 +26,30 @@ class TestBooksCollector:
     @pytest.mark.parametrize(
         'book_name', [
             '',
-            'Жизнь и удивительные приключения Робинзона Крузо, моряка из Йорка, '
-            'прожившего двадцать восемь лет в полном одиночестве на необитаемом '
-            'острове у берегов Америки, близ устьев реки Ориноко, куда он был '
-            'выброшен кораблекрушением, во время которого весь экипаж корабля, '
-            'кроме него, погиб, с изложением его неожиданного освобождения пиратами; '
-            'написанные им самим'
+            'Заветы предательства. (сборник рассказов)'
         ],
-        ids=['empty len', 'over 40 len']
+        ids=['len < 1', 'len > 40']
     )
-    def test_add_new_book_incorrect_length_not_added(self,book_name):
-        collector = BooksCollector()
+    def test_add_new_book_incorrect_length_not_added(self,book_name, collector):
         collector.add_new_book(book_name)
 
         assert not collector.get_books_genre()
 
+    @pytest.mark.parametrize(
+        'book_name',
+        [
+            '1',
+            '1234567890123456789012345678901234567890'
+        ],
+        ids=['length = 1','length = 40']
+    )
+    def test_add_new_book_correct_length_added(self, book_name, collector):
+        collector.add_new_book(book_name)
 
-    def test_add_new_book_duplicate_not_added(self):
-        collector = BooksCollector()
+        assert book_name in collector.get_books_genre()
+
+
+    def test_add_new_book_duplicate_not_added(self, collector):
         collector.add_new_book('Возвышение Хоруса')
         collector.add_new_book('Возвышение Хоруса')
 
@@ -59,16 +61,14 @@ class TestBooksCollector:
         [('Лживые Боги', 'Комедии'), ('Полёт Эйзенштейна', 'Мультфильмы')],
         ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
     )
-    def test_set_book_genre_set_existing_genre(self, book_name, book_genre):
-        collector = BooksCollector()
+    def test_set_book_genre_set_existing_genre(self, book_name, book_genre, collector):
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, book_genre)
 
         assert collector.get_book_genre(book_name) == book_genre
 
 
-    def test_set_book_genre_set_non_existing_genre_fail(self):
-        collector = BooksCollector()
+    def test_set_book_genre_set_non_existing_genre_fail(self, collector):
         collector.add_new_book('Лживые Боги')
         collector.set_book_genre('Лживые Боги', 'Сборник рецептов')
 
@@ -80,8 +80,7 @@ class TestBooksCollector:
         [('Лживые Боги', 'Комедии'), ('Полёт Эйзенштейна', 'Мультфильмы')],
         ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
     )
-    def test_get_book_genre_returns_genre(self,book_name, book_genre):
-        collector = BooksCollector()
+    def test_get_book_genre_returns_genre(self,book_name, book_genre, collector):
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, book_genre)
         returned_genre = collector.get_book_genre(book_name)
@@ -95,9 +94,8 @@ class TestBooksCollector:
         ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
     )
     def test_get_books_with_specific_genre_existing_book_returns_book(
-            self, book_name, book_genre
+            self, book_name, book_genre, collector
     ):
-        collector = BooksCollector()
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, book_genre)
         books_by_genre = collector.get_books_with_specific_genre(book_genre)
@@ -110,8 +108,7 @@ class TestBooksCollector:
         [('Лживые Боги', 'Гримдарк'), ('Полёт Эйзенштейна', 'Эпос')],
         ids=['False Gods + Grimdark','The Flight of the Eisenstein + Epic Poetry']
     )
-    def test_get_books_with_specific_genre_incorrect_genre_returns_empty_list(self, book_name, book_genre):
-        collector = BooksCollector()
+    def test_get_books_with_specific_genre_incorrect_genre_returns_empty_list(self, book_name, book_genre, collector):
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, book_genre)
         books_by_genre = collector.get_books_with_specific_genre(book_genre)
@@ -125,16 +122,10 @@ class TestBooksCollector:
             (
                     {'Лживые Боги': 'Комедии',
                      'Полёт Эйзенштейна': 'Мультфильмы'}
-            ),
-            (
-                    {'Возвышение Хоруса': 'Детективы',
-                     'Тысяча Сынов': 'Комедии'}
             )
-        ],
-        ids=['False Gods + Comedy','The Flight of the Eisenstein + Animation']
+        ]
     )
-    def test_get_books_genre_add_2_books_return_2_books(self, bookngenre):
-        collector = BooksCollector()
+    def test_get_books_genre_add_2_books_return_2_books(self, bookngenre, collector):
         for k, v in bookngenre.items():
             collector.add_new_book(k)
             collector.set_book_genre(k, v)
@@ -143,8 +134,7 @@ class TestBooksCollector:
         assert books_by_genre == bookngenre
 
 
-    def test_get_books_genre_no_books_return_empty_dict(self):
-        collector = BooksCollector()
+    def test_get_books_genre_no_books_return_empty_dict(self, collector):
         books_by_genre = collector.get_books_genre()
 
         assert books_by_genre == {}
@@ -155,8 +145,7 @@ class TestBooksCollector:
         [('Лживые Боги', 'Комедии'), ('Полёт Эйзенштейна', 'Мультфильмы')],
         ids=['Comedy','Animation']
     )
-    def test_get_books_for_children_add_children_books_return_children_books(self, book_name, book_genre):
-        collector = BooksCollector()
+    def test_get_books_for_children_add_children_books_return_children_books(self, book_name, book_genre, collector):
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, book_genre)
         children_books = collector.get_books_for_children()
@@ -169,8 +158,7 @@ class TestBooksCollector:
         [('Где Уолли?', 'Детективы'), ('Дом на Пуховой опушке', 'Ужасы')],
         ids=['Detective', 'Horror']
     )
-    def test_get_books_for_children_add_non_children_books_return_empty_list(self, book_name, book_genre):
-        collector = BooksCollector()
+    def test_get_books_for_children_add_non_children_books_return_empty_list(self, book_name, book_genre, collector):
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, book_genre)
         children_books = collector.get_books_for_children()
@@ -178,8 +166,7 @@ class TestBooksCollector:
         assert children_books == []
 
 
-    def test_add_book_in_favorites_add_1_fav_book_return_1_fav_book(self):
-        collector = BooksCollector()
+    def test_add_book_in_favorites_add_1_fav_book_return_1_fav_book(self, collector):
         book_name = 'Сожжение Просперо'
         collector.add_new_book(book_name)
         collector.add_book_in_favorites(book_name)
@@ -187,8 +174,7 @@ class TestBooksCollector:
         assert collector.favorites == [book_name]
 
 
-    def test_delete_book_from_favorites_add_1_delete_1_return_empty_favlist(self):
-        collector = BooksCollector()
+    def test_delete_book_from_favorites_add_1_delete_1_return_empty_favlist(self, collector):
         book_name = 'Сожжение Просперо'
         collector.add_new_book(book_name)
         collector.add_book_in_favorites(book_name)
@@ -197,8 +183,7 @@ class TestBooksCollector:
         assert collector.favorites == []
 
 
-    def test_get_list_of_favorites_books_added_2_fav_return_2_fav(self):
-        collector = BooksCollector()
+    def test_get_list_of_favorites_books_added_2_fav_return_2_fav(self, collector):
         book1_name = 'Сожжение Просперо'
         book2_name = 'Фулгрим'
         collector.add_new_book(book1_name)
